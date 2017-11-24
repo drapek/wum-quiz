1067d2feaa17f5ca0facbe2ee9cbacc11484d6f4
Paweł Drapiewski <drapek39@wp.pl>
2017-11-24 22:54:55 +0100

    Init commit - implemented project structure.

---
+++ README.md
@@ -0,0 +1,9 @@
+# QUIZ WUM
+
+## Introduction
+
+This is app to help memorize latin names of bacteria
+
+## Requirements
+* Python 3.6
+* Flask 0.12.2
---
+++ app/views.py
@@ -0,0 +1,26 @@
+from app import app
+
+
+@app.route('/')
+def index():
+    # TODO implement homepage
+    return 'Not implemented yet.'
+
+
+@app.route('/rand_element')
+def rand_elem():
+    # TODO implement this
+    return 'Not implemented yet'
+
+
+@app.route('/manage')
+def manage():
+    # TODO implement this
+    return 'Not implemented yet'
+
+
+@app.route('/list_all')
+def list_all():
+    # TODO implement this
+    return 'Not implemented yet'
+
