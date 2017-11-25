bae53b24bd728ee6e159dfa5145ccde2023fad1e
Paweł Drapiewski <drapek39@wp.pl>
2017-11-25 14:27:36 +0100

    Add basic bootstrap stylish.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask('app')
 app.config.from_object('app.config')
 db = SQLAlchemy(app)
-
+Bootstrap(app)
 
 from app import views, models
